db79f44ddc79a46b4d3dffc93dcb635699c8eb4a
Czasopismo MATINF <140089762+czasopismo-MATINF@users.noreply.github.com>
2023-08-29 08:19:36 +0200

    Add files via upload

---
+++ kurs języka Python (.2023)/skrypt numer 1.py
@@ -0,0 +1,30 @@
+print("Obliczenia")
+
+sekundy = (100*365+(100/25))*24*60*60
+print(sekundy)
+sekundy = (100*365+(100/4))*24*60*60
+print(sekundy)
+sekundy = (100*365+(100//4))*24*60*60
+print(sekundy)
+
+print("\nNapisy")
+
+napis = "Matematyka Informatyka"
+print(napis[0:10])
+print(napis[0:11])
+print(napis[0:12])
+
+#print(napis[0:2:10])
+#print(napis[range[0:2:10]])
+#print(napis[range(0,2,10)])
+
+print("\nListy")
+
+lista = ["Matematyka", "Informatyka", "Technika", "Programowanie", "Architektura", "Literatura"]
+print(lista[1])
+print(lista[2:5])
+print(lista[1:5])
+
+#print(lista[range(1,2,5)])
+
+
